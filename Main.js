02a5eeee84ff48b968baeeed5a862d9a24999de5
emedee <emediongini@gmail.com>
2020-01-29 11:33:31 -1200

    'commit'

--- Main.js
+++ Main.js
@@ -3,15 +3,13 @@ let time = document.querySelector('#input2');
 let rate = document.querySelector('#input3');
 let interest = document.querySelector('#span');
 let calculate = document.querySelector('#calculate')
-let solutionField = document.querySelector('.solutionField');
-
+let solutionField = document.querySelector('.mlto')
 let inputFields = [principal, time, rate];
 
 calculate.addEventListener('click', function(){
     for(let i=0; i<inputFields.length; i++){
         if(inputFields[i].value < 1){
             inputFields[i].style.border = '2px solid red';
-            interest.innerHTML = '';
             solutionField.style.display = 'none';
 
             // Sweet Alert pop-up
@@ -21,11 +19,11 @@ calculate.addEventListener('click', function(){
                 text: 'Enter a valid Input!',
             })
 
-        } else if((inputFields[i].value > 1)){
+        } else {
         let solution = `N${(principal.value * time.value * rate.value)/100}`;
         interest.innerHTML = solution;
         inputFields[i].style.border = '';
-        solutionField.style.display = 'initial';
+        solutionField.style.display = 'block';
         }
     }
 })
